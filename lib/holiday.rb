678783ab36772645bc8d99975914c7c6b90966cc
Kizito Njoku <nkizito57@gmail.com>
2019-11-09 06:32:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,11 +39,11 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-  holiday_hash.merge {
+  holiday_hash.merge( {
     :season => {
       :holiday_name => supply_array
     }
-  }
+  })
   holiday_hash
 end 
 
