0a6d8ce2175964b42104afa37ea5f0ff3cdc51b8
Kizito Njoku <nkizito57@gmail.com>
2019-11-09 06:40:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,9 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    if season == :winter || season == :summer
       puts holiday
-    end 
   end
 end
 
