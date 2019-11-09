4be45a0f4469079f7f20d4cc9d14a3542fd610ba
Kizito Njoku <nkizito57@gmail.com>
2019-11-09 06:43:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-      p season
+      p season 
+      p holiday
   end
 end
 
