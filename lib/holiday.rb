bee5de740e66e249a69ea95488ce51290f646f92
Kizito Njoku <nkizito57@gmail.com>
2019-11-09 07:19:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.each do |season, holiday|
-    season
+    holiday if include? = "BBQ"
   end
 end
 
